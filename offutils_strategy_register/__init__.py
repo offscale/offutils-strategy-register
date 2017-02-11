b450cbc4a4df5ee5f638fdd915a2b49ab2329e20
Samuel Marks <samuelmarks@gmail.com>
2017-02-11 15:50:35 +1100

    VagrantNodeDriver

--- offutils_strategy_register/__init__.py
+++ offutils_strategy_register/__init__.py
@@ -13,8 +13,10 @@ except ImportError:
     import pickle
 
 from libcloud.compute.base import NodeImage, NodeLocation, NodeSize
+# TODO: Automatically import the right ones rather than listing:
 from libcloud.compute.drivers.azure import AzureNodeLocation
 from libcloud.compute.drivers.ec2 import EC2NodeLocation
+from libcloud.compute.drivers.vagrant import VagrantNodeDriver
 
 __author__ = 'Samuel Marks'
 __version__ = '0.0.6'
